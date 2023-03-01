5b6e96d78b4c94ab6e18a0ce2ff991f3bda056e5
Gabriel Queiroz <gqueiroz_photo@hotmail.com>
2023-03-01 16:42:36 -0300

    test: fix tests for hosting module

--- src/hosting/hosting.controller.spec.ts
+++ src/hosting/hosting.controller.spec.ts
@@ -1,16 +1,20 @@
+import { getQueueToken } from '@nestjs/bull';
 import { Test, TestingModule } from '@nestjs/testing';
 import { HostingController } from './hosting.controller';
+import { HostingModule } from './hosting.module';
 import { HostingService } from './hosting.service';
 
 describe('HostingController', () => {
   let controller: HostingController;
+  let service: HostingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [HostingController],
-      providers: [HostingService],
-    }).compile();
-
+      imports: [HostingModule],
+    }).overrideProvider(getQueueToken("terraform"))
+      .useValue("terraform")
+      .compile();
+    service = module.get<HostingService>(HostingService);
     controller = module.get<HostingController>(HostingController);
   });
 
